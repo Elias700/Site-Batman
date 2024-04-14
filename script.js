c4ae71ce17da3366e948828cb0d6ad99b70b24da
Elias700 <elias_ribeiro07@outlook.com.br>
2024-04-14 18:04:11 -0300

    fotos concluido

---
+++ script.js
@@ -0,0 +1,15 @@
+
+
+
+
+
+const fotos = [
+    {title: '', img: '', descricao: ''},
+    {title: '', img: '', descricao: ''},
+    {title: '', img: '', descricao: ''},
+    {title: '', img: '', descricao: ''},
+    {title: '', img: '', descricao: ''},
+];
+
+const btt = document.getElementsByClassName('conteudoPrincipal')
+
